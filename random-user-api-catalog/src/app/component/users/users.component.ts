11fcec9a3f37b34f49716248ab2b7fdf8f13e2ec
TomaszChylinski <chylinski.t@outlook.com>
2022-07-04 12:21:12 -0400

    feat(my-app): configure styles

--- random-user-api-catalog/src/app/component/users/users.component.ts
+++ random-user-api-catalog/src/app/component/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../shared/service/user.service';
+import { Response } from '../../interface/response.interface';
 
 @Component({
   selector: 'app-users',
@@ -7,11 +8,13 @@ import { UserService } from '../../shared/service/user.service';
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  userData: Response;
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.userService.getUsers(15).subscribe((results) => {
       console.log('show me data', results);
+      this.userData = results;
     });
   }
 }
